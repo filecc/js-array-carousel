70aa4c3f1c045b8ab7bbb0bd7cf22f6140807072
filecc <fil.gia@live.it>
2023-03-14 16:14:56 +0100

    completed thumbnails

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ appHeight();
 const images = ["01", "02", "03", "04", "05"];
 const wrapper = document.getElementById("wrapper");
 
+
 let slide = "";
 
 for (let i = 0; i < images.length; i++) {
@@ -24,6 +25,7 @@ for (let i = 0; i < images.length; i++) {
 }
 
 wrapper.innerHTML += slide;
+
 
 const slides = document.querySelectorAll(".slide");
 let current = 0;
@@ -44,9 +46,34 @@ prev.addEventListener("click", () => {
   addActive();
 });
 
+
+/* thumbanil */
+
+let thumb = '';
+for(let i=0;i<images.length;i++){
+    thumb+= `
+    <div class="thumbs ${i}">
+        <img src='./img/${images[i]}.webp' alt='image-${images[i]}'>     
+    </div>
+    `;
+}
+const thumbnail = document.querySelector('.thumbnails');
+
+thumbnail.innerHTML += thumb;
+
+const thumbs = document.querySelectorAll('.thumbs');
+thumbs[current].classList.add('active');
+
+
 function removeActive() {
   slides[current].classList.remove("active");
+  thumbs[current].classList.remove('active');
+  thumbnail.scrollTo(current * 80 + current, 0)
 }
 function addActive() {
   slides[current].classList.add("active");
+  thumbs[current].classList.add('active');
+  thumbnail.scrollTo(current * 80 + current, 0)
 }
+
+
