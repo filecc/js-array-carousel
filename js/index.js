e9763489d747d44fdfef238218d443f57b17d08c
filecc <fil.gia@live.it>
2023-03-14 15:03:20 +0100

    removed unnecessary functions

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
 /* fixing window height on iPhone */
 
 const appHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
+  document.documentElement.style.setProperty(
+    "--app-height",
+    `${window.innerHeight}px`
+  );
+};
+window.addEventListener("resize", appHeight);
+appHeight();
 /* fixing window height on iPhone */
 
-const images = ['01', '02', '03', '04', '05']
-const wrapper = document.getElementById('wrapper');
+const images = ["01", "02", "03", "04", "05"];
+const wrapper = document.getElementById("wrapper");
 
-let slide = '';
+let slide = "";
 
-for (let i= 0; i<images.length; i++){
-    slide += `
+for (let i = 0; i < images.length; i++) {
+  slide += `
     <div class="slide">
         <img src='./img/${images[i]}.webp' alt='image-${images[i]}'>     
     </div>
@@ -23,53 +25,28 @@ for (let i= 0; i<images.length; i++){
 
 wrapper.innerHTML += slide;
 
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll(".slide");
 let current = 0;
-slides[current].classList.add('active');
+slides[current].classList.add("active");
 
-const next = document.querySelector('#wrapper .next');
-const prev = document.querySelector('#wrapper .prev');
+const next = document.querySelector("#wrapper .next");
+const prev = document.querySelector("#wrapper .prev");
 
+next.addEventListener("click", () => {
+  removeActive();
+  current = (current + 1) % images.length;
+  addActive();
+});
 
+prev.addEventListener("click", () => {
+  removeActive();
+  current = (current - 1 + images.length) % images.length;
+  addActive();
+});
 
-next.addEventListener('click', () => {
-    if (current < images.length - 1) {
-        moveForward();
-    } else {
-        removeActive();
-        current = 0;
-        addActive();
-    }
-    
-})
-prev.addEventListener('click', () => {
-    if (current === 0) {
-        removeActive();
-        current = images.length - 1;
-        addActive();
-    } else {
-       moveBackward();
-    }
-    
-})
-
-
-function moveForward(){
-    removeActive();
-    current++;
-    addActive();
+function removeActive() {
+  slides[current].classList.remove("active");
 }
-
-function moveBackward(){
-    removeActive();
-    current--;
-    addActive();
+function addActive() {
+  slides[current].classList.add("active");
 }
-
-function removeActive(){
-    slides[current].classList.remove('active');
-}
-function addActive(){
-    slides[current].classList.add('active');
-}
-
